7236ee74a04ad7b46515913e97dded7586309ad4
Kenshi Muto <kmuto@kmuto.jp>
2023-08-02 22:41:46 +0900

    tick 1

--- agent/agent.go
+++ agent/agent.go
@@ -36,36 +36,52 @@ func (agent *Agent) CollectMetrics(collectedTime time.Time) *MetricsResult {
 	return &MetricsResult{Created: collectedTime, Values: values}
 }
 
+func ticktuck(result chan time.Time, from time.Time, to time.Time, done chan struct{}) {
+	from_t := from.Unix()
+	to_t := to.Unix()
+	for t := from_t; t < to_t; t++ {
+		result <- time.Unix(t, 0)
+	}
+	done <- struct{}{}
+}
+
 // Watch XXX
-func (agent *Agent) Watch(ctx context.Context) chan *MetricsResult {
+func (agent *Agent) Watch(ctx context.Context, commandTicker chan time.Time) chan *MetricsResult {
 
 	metricsResult := make(chan *MetricsResult)
 	ticker := make(chan time.Time)
 	interval := config.PostMetricsInterval
 
 	go func() {
-		t := time.NewTicker(1 * time.Second)
+		from := time.Date(2023, 7, 28, 03, 33, 0, 0, time.Local)
+		// .Add(time.Second * 1)
+		to := time.Date(2023, 7, 28, 18, 0, 0, 0, time.Local)
+		//t := time.NewTicker(1 * time.Second)
+		t := make(chan time.Time)
+		done := make(chan struct{})
+		go ticktuck(t, from, to, done)
 
-		last := time.Now()
+		last := from
 		ticker <- last // sends tick once at first
 
 		for {
 			select {
 			case <-ctx.Done():
 				close(ticker)
-				t.Stop()
+				close(t)
 				return
-			case t := <-t.C:
+			case ti := <-t:
+				commandTicker <- ti
 				// Fire an event at 0 second per minute.
 				// Because ticks may not be accurate,
 				// fire an event if t - last is more than 1 minute
-				if t.Second()%int(interval.Seconds()) == 0 || t.After(last.Add(interval)) {
+				if ti.Second()%int(interval.Seconds()) == 0 || ti.After(last.Add(interval)) {
 					// Non-blocking send of time.
 					// If `collectMetrics` runs with max concurrency, we drop ticks.
 					// Because the time is used as agent.MetricsResult.Created.
 					select {
-					case ticker <- t:
-						last = t
+					case ticker <- ti:
+						last = ti
 					default:
 					}
 				}
